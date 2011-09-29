af4ee8b5b5182db4a1cebeb2daecdff74b2865fb
Michael Contento <michaelcontento@gmail.com>
2011-09-29 13:57:46 +0200

    Indentation fixed

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,13 +2,13 @@
 
 function autoloader($class) 
 {
-	require str_replace('_', '/', $class) . '.php';
+    require str_replace('_', '/', $class) . '.php';
 }
 
 spl_autoload_register('autoloader');
 
 set_include_path(
-  dirname(__FILE__) . '/../lib/'. PATH_SEPARATOR 
-  . get_include_path()
+    dirname(__FILE__) . '/../lib/'. PATH_SEPARATOR 
+    . get_include_path()
 );
 
